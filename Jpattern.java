def82d9c54540faecb606b742e2d020004396ebb
Akash singh <83067464+kshatriyaakash@users.noreply.github.com>
2021-10-29 21:00:27 +0530

    Pattern

---
+++ Jpattern.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Jpattern
+
+{            
+
+    {
+
+        int alphabet = 65;
+
+                for (int i = 0; i <= 5; i++)
+
+        {
+
+            for (int j = 0; j <= i; j++)
+
+            {
+
+                System.out.print((char) (alphabet + j) + ” “);
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
